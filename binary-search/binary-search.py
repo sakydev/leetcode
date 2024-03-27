0daf975a19917646144344989c700c2f0fa77a92
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-03-27 22:21:05 +0100

    add binary search python

---
+++ binary-search/binary-search.py
@@ -0,0 +1,19 @@
+import math
+def search(nums, target):
+    start = 0
+    end = len(nums) - 1
+
+    while (start <= end):
+        middle = math.floor((start + end) / 2)
+
+        if nums[middle] == target:
+            return middle
+
+        if target > nums[middle]:
+            start = middle + 1
+        else:
+            end = middle - 1
+
+    return -1
+
+print(search([-1,0,3,5,9,12], 9))
