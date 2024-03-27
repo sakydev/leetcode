46ce7352bb528f5b06b1bb8df2cdc04e510d7d96
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-03-27 22:22:42 +0100

    add valid parentheses solution in js

---
+++ valid-parentheses/valid-parentheses.js
@@ -0,0 +1,25 @@
+let isValid = function(s) {
+    const endToStart = {')': '(', ']': '[', '}': '{'}
+    let stack = []
+
+    const crumbs = s.split('')
+    for (let i = 0; i < crumbs.length; i++) {
+        let current = crumbs[i]
+        if (stack.length && current in endToStart) {
+            if (stack[stack.length - 1] === endToStart[current]) {
+                stack.pop()
+
+                continue
+            }
+
+            return false
+        }
+
+        stack.push(current)
+    }
+
+    return !stack.length
+};
+
+//console.log(isValid('()'))
+console.log(isValid('()[]{}'))
