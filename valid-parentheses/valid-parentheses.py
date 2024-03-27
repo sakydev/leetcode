93b5fd6e7473660bec261813645f1362accae848
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-03-27 22:22:53 +0100

    add valid parentheses solution in python

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+def isValid(s):
+    endToStart = {')': '(', ']': '[', '}': '{'}
+    stack = []
+
+    for current in s:
+        if current in endToStart:
+            if stack and stack[-1] == endToStart[current]:
+                stack.pop()
+
+                continue
+
+            return False
+
+        stack.append(current)
+
+    return len(stack) < 1
+
+print(isValid('())[]{}'))
